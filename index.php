c0f6463b2f7772b7bf0a1f4c27a64f58b23250de
Ginazai <rafaeldc1300@gmail.com>
2024-05-17 22:36:50 -0500

    added base html

--- index.php
+++ index.php
@@ -26,14 +26,35 @@ while($get_data=$data->fetch(PDO::FETCH_ASSOC)){$all_data[]=$get_data;}
     crossorigin="anonymous">
   </head>
   <body>
+    <div class="container">
+      <button class='btn btn-dark my-3 float-end' type='submit' data-bs-toggle='modal' data-bs-target='#add-modal'>+ Agregar</button>
+      <div class="row w-100">
     <?php
     if(count($all_data)>0){
       foreach($all_data as $account){
-        //here goes the html
+        ?>
+        <div class="my-2">
+          <div class="accordion" id="accordionExample">
+            <div class="accordion-item">
+              <h2 class="accordion-header">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+                  Accordion Item #1
+                </button>
+              </h2>
+              <div id="collapseTwo" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                  <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <?php
       }
     }   
     ?>
-
+      </div>
+    </div>
   <script 
   src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" 
   integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" 
@@ -45,5 +66,65 @@ while($get_data=$data->fetch(PDO::FETCH_ASSOC)){$all_data[]=$get_data;}
   <!-- my resources -->
   <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
   <script type="text/javascript" src="js/script.js"></script>
+
+  <!---------------------------------------------- Add modal ---------------------------------------------->
+  <div class='modal fade' id='add-modal' tabindex='-1' aria-labelledby='modal-label' aria-hidden='true'>
+    <div class='modal-dialog modal-dialog-centered'>
+      <div class='modal-content'>
+        <div class='modal-header bg-dark'>
+          <h1 class='modal-title fs-5 text-white' id='modal-label'>Agregar usuario</h1>
+        </div>
+        <div class='modal-body'>
+          <div class='container-fluid justify-content-center form-signin'>
+      <!--------------------------Add Form -------------------------->
+            <form id='user-add' class='row g-3' role='form' name='user-add' action='actions/create/crear_usuario.php' method='post'>
+
+              <div class='row g-2'>
+                <div class='form-floating'>
+                  <input type='text' name='name' id='name' class='form-control' placeholder='Nombre'>
+                  <label for='name'>Nombre</label>
+                </div>
+                <div class='form-floating'>
+                  <input type='text' name='lastname' id='lastname' class='form-control' placeholder='Apellido'>
+                  <label for='lastname'>Apellido</label>
+                </div>
+              </div>
+              <div class='form-floating'>
+                <input class='form-control' type='date' name='dob' id='dob' placeholder='Fecha de nacimiento'>
+                <label for='dob'>Fecha de nacimiento</label>
+              </div>
+
+              <div class='form-floating'>
+                <input type='password' name='password' id='password' class='form-control' placeholder='Contrase&ntilde;a'>
+                <label for='password'>Contrase&ntilde;a</label>
+              </div>
+              <div class='input-group'>
+                <span id='phone-label' class='input-group-text'>+507</span>
+                <input type='number' name='phone' id='phone' class='form-control' placeholder='Numero de telefono' aria-describedby='phone-label'>
+              </div>
+              <div class='form-floating'>
+                <input type='email' name='email' id='email' class='form-control' placeholder='Correo Electronico'>
+                <label for='email'>Correo Electronico</label>
+              </div>
+
+              <div class='checkbox-inline'>
+                <label>
+                  <input type='checkbox' name='active' value='1' checked> Active
+                </label>
+              </div>
+              
+            </form>
+  <!--------------------------Add Form -------------------------->
+          </div>
+        </div>
+
+        <div class='modal-footer bg-dark'>
+          <button type='submit' form='user-add' name='form-add-submit' class='btn btn-info'>Agregar</button>
+          <button type='button' class='btn btn-secondary' data-bs-dismiss='modal'>Cerrar</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--------------------------Add modal -------------------------->
   </body>
 </html>
