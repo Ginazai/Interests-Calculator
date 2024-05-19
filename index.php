c9c3cd11289cc544835d388f386ab39c6be13130
Ginazai <rafaeldc1300@gmail.com>
2024-05-18 23:09:06 -0500

    added aesthetic elements including data insertion from db. Need to work on math logic for calculating interests,

--- index.php
+++ index.php
@@ -4,6 +4,11 @@ require_once 'php/connection.php';
 $data=$con->prepare("SELECT * FROM accounts");
 $data->execute();
 while($get_data=$data->fetch(PDO::FETCH_ASSOC)){$all_data[]=$get_data;}
+
+$payments=$con->prepare("SELECT * FROM payments");
+$payments->execute();
+while($payment_row=$payments->fetch(PDO::FETCH_ASSOC)){$all_payments[]=$payment_row;}
+$all_payments=json_encode($all_payments, JSON_PRETTY_PRINT);
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -24,6 +29,7 @@ while($get_data=$data->fetch(PDO::FETCH_ASSOC)){$all_data[]=$get_data;}
     rel="stylesheet" 
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" 
     crossorigin="anonymous">
+    <?= isset($all_payments) ? "<script type='text/javascript'>var all_payments=$all_payments;</script" : "" ?>
   </head>
   <body>
     <div class="container">
@@ -32,18 +38,84 @@ while($get_data=$data->fetch(PDO::FETCH_ASSOC)){$all_data[]=$get_data;}
     <?php
     if(count($all_data)>0){
       foreach($all_data as $account){
+        $id=$account['account_id'];
+        $account_name=$account['account_name'];
+        $borrow=$account['borrow_amount'];
+        $owner=$account['owner'];
+        $create_date=date('d-m-Y',strtotime($account['create_date']));
+        $active=$account['active'];
         ?>
         <div class="my-2">
-          <div class="accordion" id="accordionExample">
+          <div class="accordion" id="data-accordion-<?= $id ?>">
             <div class="accordion-item">
               <h2 class="accordion-header">
-                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-                  Accordion Item #1
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-<?= $id ?>" aria-expanded="false" aria-controls="collapse-<?= $id ?>">
+                  <div class="d-flex justify-content-between w-100">
+                    <div class="row">
+                      <h3><?= $account_name ?></h3>
+                      <p><?= $owner ?></p>
+                    </div>
+                    <div class="ms-auto me-3 h-100 my-auto">
+                      <div>
+                        <div class="col-12"><p class="float-end my-0">$<?= $borrow ?></p></div>
+                        <div class="col-12"><p class="float-end my-0" ><?= $create_date ?></p></div>
+                        <div class="col-12"><b><p class="float-end my-0"><?=$active==1?"active":"inactive"?></p></b></div>
+                      </div>
+                    </div>                
+                  </div>
                 </button>
               </h2>
-              <div id="collapseTwo" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+              <div id="collapse-<?= $id ?>" class="accordion-collapse collapse" data-bs-parent="#data-accordion-<?= $id ?>">
                 <div class="accordion-body">
-                  <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                  <button class='btn btn-dark btn-sm' type='submit' data-bs-toggle='modal' data-bs-target='#add-payment-<?=$id?>'>+ Agregar pago</button> 
+
+                  <table class="table table-striped">
+                    <thead>
+                      <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Fecha</th>
+                        <th scope="col">Balance</th>
+                        <th scope="col">Intereses</th>
+                        <th scope="col">Pago</th>
+                        <th scope="col">Nuevo Balance</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <?php 
+                      $get_history=$con->prepare("SELECT accounts.borrow_amount, payments.*
+                                                  FROM accounts
+                                                  JOIN payments
+                                                  ON accounts.account_id=payments.account_id
+                                                  WHERE accounts.account_id=:cid
+                                                  ORDER BY DATE(accounts.create_date)");
+                      $get_history->execute([':cid'=>$id]);
+                      $history=array();
+                      while($history_row=$get_history->fetch(PDO::FETCH_ASSOC)){$history[]=$history_row;}
+                      if(count($history)>0){
+                        $last_amount=$borrow;
+                        foreach($history as $element){
+                          $date_gap=(intval(date('d',strtotime($element['payment_date'])))-intval(date('d', strtotime($create_date))));
+                          $payment_id=$element['payment_id'];
+                          $payment_amount=$element['amount'];
+                          $payment_date=date('d-m-Y',strtotime($element['payment_date']));
+
+                          // echo "date gap: " . $date_gap . "<br>";
+                          ?>
+                          <tr>
+                            <th scope="row"><?=$payment_id?></th>
+                            <td><?=$payment_date?></td>
+                            <td><?= $last_amount ?></td>
+                            <td><?= $interests = round($last_amount*(0.6/365),2) ?></td>
+                            <td><?= $payment_amount ?></td>
+                            <td><?=$last_amount-=round($payment_amount+$interests,2)?></td>
+                          </tr>
+                          <?php
+                        }
+                      }
+                      ?>
+                    </tbody>
+                  </table>
+
                 </div>
               </div>
             </div>
@@ -72,45 +144,30 @@ while($get_data=$data->fetch(PDO::FETCH_ASSOC)){$all_data[]=$get_data;}
     <div class='modal-dialog modal-dialog-centered'>
       <div class='modal-content'>
         <div class='modal-header bg-dark'>
-          <h1 class='modal-title fs-5 text-white' id='modal-label'>Agregar usuario</h1>
+          <h1 class='modal-title fs-5 text-white' id='modal-label'>Agregar cuenta</h1>
         </div>
         <div class='modal-body'>
           <div class='container-fluid justify-content-center form-signin'>
       <!--------------------------Add Form -------------------------->
             <form id='user-add' class='row g-3' role='form' name='user-add' action='actions/create/crear_usuario.php' method='post'>
 
-              <div class='row g-2'>
+  
                 <div class='form-floating'>
                   <input type='text' name='name' id='name' class='form-control' placeholder='Nombre'>
-                  <label for='name'>Nombre</label>
+                  <label for='name'>Nombre de la cuenta</label>
                 </div>
                 <div class='form-floating'>
                   <input type='text' name='lastname' id='lastname' class='form-control' placeholder='Apellido'>
-                  <label for='lastname'>Apellido</label>
+                  <label for='lastname'>Acreedor</label>
                 </div>
+
+              <div class='form-floating'>
+                <input type='number' name='balance' id='balance' class='form-control' placeholder='Numero de telefono' aria-describedby='phone-label'>
+                <label for='balance'>Monto solicitado</label>
               </div>
               <div class='form-floating'>
                 <input class='form-control' type='date' name='dob' id='dob' placeholder='Fecha de nacimiento'>
-                <label for='dob'>Fecha de nacimiento</label>
-              </div>
-
-              <div class='form-floating'>
-                <input type='password' name='password' id='password' class='form-control' placeholder='Contrase&ntilde;a'>
-                <label for='password'>Contrase&ntilde;a</label>
-              </div>
-              <div class='input-group'>
-                <span id='phone-label' class='input-group-text'>+507</span>
-                <input type='number' name='phone' id='phone' class='form-control' placeholder='Numero de telefono' aria-describedby='phone-label'>
-              </div>
-              <div class='form-floating'>
-                <input type='email' name='email' id='email' class='form-control' placeholder='Correo Electronico'>
-                <label for='email'>Correo Electronico</label>
-              </div>
-
-              <div class='checkbox-inline'>
-                <label>
-                  <input type='checkbox' name='active' value='1' checked> Active
-                </label>
+                <label for='dob'>Fecha de inicio</label>
               </div>
               
             </form>
--- model.sql
+++ model.sql
@@ -3,6 +3,7 @@ use interets_calculator;
 create table accounts(
 	account_id int auto_increment primary key not null,
 	account_name varchar(100) not null,
+	borrow_amount float not null,
 	owner varchar(100) not null,
 	create_date datetime not null,
 	active boolean not null DEFAULT 1
@@ -20,6 +21,9 @@ create table payments(
 	on update cascade
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 --test values
-insert into accounts(account_name,owner,create_date,active) values ('sample account','Sample owner','01-01-2024 00:00:00',1);
-insert into accounts(account_name,owner,create_date,active) values ('sample account 2','Sample owner 2','01-01-2024 00:00:00',1);
-insert into accounts(account_name,owner,create_date,active) values ('sample account 3','Sample owner 3','01-01-2024 00:00:00',1);
+insert into accounts(account_name,borrow_amount,owner,create_date,active) values ('sample account',300.00,'Sample owner','01-01-2024 00:00:00',1);
+insert into accounts(account_name,borrow_amount,owner,create_date,active) values ('sample account 2',500.00,'Sample owner 2','01-01-2024 00:00:00',1);
+insert into accounts(account_name,borrow_amount,owner,create_date,active) values ('sample account 3',400.00,'Sample owner 3','01-01-2024 00:00:00',1);
+
+insert into payments(amount,payment_date,account_id) values(160.25,'0000-00-00 00:00:00',1);
+insert into payments(amount,payment_date,account_id) values(50.25,'0000-00-00 00:00:00',1);
