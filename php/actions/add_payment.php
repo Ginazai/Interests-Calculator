8abdd903725a4ebe1bb8c70ea736a9bdf2e356c9
Ginazai <rafaeldc1300@gmail.com>
2024-09-13 17:33:42 -0500

    partially completed invidual csv generation

--- php/actions/add_payment.php
+++ php/actions/add_payment.php
@@ -5,7 +5,7 @@ require_once '../connection.php';
 if(isset($_POST)){
 	$id=$_GET['id'];
 
-	$insert_payment = $con->prepare("INSERT INTO payments (account_id,amount,payment_date) VALUES (:ai,:am,:pd)");
+	$insert_payment = $con->prepare("INSERT INTO payments (account_id,amount,create_date) VALUES (:ai,:am,:pd)");
 	$insert_payment->execute([
 		":ai"=> $id,
 		":am"=> $_POST['payment_amount'],
